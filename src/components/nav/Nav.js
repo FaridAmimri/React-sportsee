552e0bcc05f9cd9c20fbfbfa7e61613d45ad76da
Farid Amimri <farid.amimri@gmail.com>
2022-02-23 11:58:41 +0100

    suppr fichiers inutiles

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,11 @@
+
+
+function Nav() {
+  return (
+    <div className='nav-horizontal'>
+        logo
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/profile.js
@@ -0,0 +1,11 @@
+
+
+function profile() {
+  return (
+   <>
+   
+   </>
+  )
+}
+
+export default profile
