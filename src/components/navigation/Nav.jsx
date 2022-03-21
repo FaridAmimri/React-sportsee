34e154a136e3317a724113060a95c836c4148f0a
Farid Amimri <farid.amimri@gmail.com>
2022-03-21 16:26:44 +0100

    modification nom composant

---
+++ src/components/navigation/Nav.jsx
@@ -0,0 +1,36 @@
+import Navtop from '../navtop/Navtop'
+import Navside from '../navside/Navside'
+import styled from 'styled-components'
+
+function Nav() {
+  return (
+    <>
+      <TopBar className="nav-top">
+        <Navtop />
+      </TopBar>
+      <SideBar className="nav-side">
+        <Navside />
+      </SideBar>
+    </>
+  )
+}
+
+export default Nav
+
+const TopBar = styled.div`
+  display: flex;
+  height: 91px;
+  min-width: 1440px;
+  background-color: #020203;
+`
+
+const SideBar = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 117px;
+  min-height: 880px;
+  position: absolute;
+  top: 90px;
+  background-color: #020203;
+`
