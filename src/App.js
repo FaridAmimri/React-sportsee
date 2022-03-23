2ef511e5738c3dedc07a714215915e69660bea3f
Farid Amimri <farid.amimri@gmail.com>
2022-03-23 09:30:45 +0100

    Suppr des props

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Nav from './components/nav/Nav'
+import Navigation from './components/navigation/Navigation'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
 
 
 function App() {
-  const [data, setData] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  // const [data, setData] = useState([])
+  // const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    fetch('http://localhost:3000/user/18')
-      .then((res) => res.json())
-      .then((res) => {
-        setData(res.data)
-        setIsLoading(false)
-      })
-  }, [])
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/user/18')
+  //     .then((res) => res.json())
+  //     .then((res) => {
+  //       setData(res.data)
+  //       setIsLoading(false)
+  //     })
+  // }, [])
 
   return (
     <>
       <BrowserRouter>
-        <Nav />
-        {isLoading && <div>Loading...</div>}
-        {!isLoading && (
+        <Navigation />
+        {/* {isLoading && <div>Loading...</div>}
+        {!isLoading && ( */}
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile data={data} />} />
+            <Route path="/user/:id" element={<Profile />} />
           </Routes>
-        )}
+        {/* )} */}
       </BrowserRouter>
     </>
   )
 }
 
 export default App
+
+// user={data}
+// data={data}
