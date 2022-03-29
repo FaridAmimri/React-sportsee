42ed0b1beb5015f34c8509e001ae677b5161d8e0
Farid Amimri <farid.amimri@gmail.com>
2022-03-29 11:23:19 +0000

    Ajout des props pour les composants enfants

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,6 @@
-/**
- * @function creating profile page with 2 links for Users
- */
-
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Model } from '../models'
 import Welcome from '../components/welcome/Welcome'
 import Activity from '../components/activity/Activity'
 import Average from '../components/average/Average'
@@ -10,25 +9,57 @@ import Score from '../components/score/Score'
 import Nutrients from '../components/nutrients/Nutrients'
 
 function Profile() {
+  const [activityData, setActivityData] = useState([])
+  const [averageData, setAverageData] = useState([])
+  const [performanceData, setPerformanceData] = useState([])
+  const [scoreData, setScoreData] = useState()
+  const [nutrientsData, setNutrientsData] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+  const { id } = useParams()
+
+  useEffect(() => {
+    Model.getUserActivity(id).then((res) => {
+      setActivityData(res.sessions)
+    })
+    Model.getUserAverage(id).then((res) => {
+      setAverageData(res.sessions)
+    })
+    Model.getUserPerformance(id).then((res) => {
+      setPerformanceData(res.data)
+    })
+    Model.getUserMainData(id).then((res) => {
+      setScoreData(res.score)
+      setNutrientsData(res.keyData)
+      setIsLoading(false)
+    })
+    setIsLoading(false)
+  }, [id])
+  
+
   return (
-    <main>
-      <div>
-        <Welcome />
-      </div>
+    <>
+      {isLoading && <div>...is loading</div>}
+      {!isLoading && (
+        <main>
+          <div>
+            <Welcome />
+          </div>
 
-      <section>
-        <article>
-          <Activity />
-          <Average />
-          <Performance />
-          <Score />
-        </article>
+          <section>
+            <article>
+              <Activity activityData={activityData} />
+              <Average averageData={averageData} />
+              <Performance performanceData={performanceData} />
+              <Score scoreData={scoreData} />
+            </article>
 
-        <div>
-          <Nutrients />
-        </div>
-      </section>
-    </main>
+            <div>
+              <Nutrients nutrientsData={nutrientsData} />
+            </div>
+          </section>
+        </main>
+      )}
+    </>
   )
 }
 
