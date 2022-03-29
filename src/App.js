0e81a1b95e76566afca842bc3d8c3d97d616f028
Farid Amimri <farid.amimri@gmail.com>
2022-03-29 11:22:32 +0000

    Props déplacé vers composant profile

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
-// import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navigation from './components/navigation/Navigation'
 import Home from './pages/Home'
 import Profile from './pages/Profile'
-
+import Navigation from './components/navigation/Navigation'
+import Layout from './components/layout/Layout'
 
 function App() {
-  // const [data, setData] = useState([])
-  // const [isLoading, setIsLoading] = useState(true)
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3000/user/18')
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       setData(res.data)
-  //       setIsLoading(false)
-  //     })
-  // }, [])
-
   return (
     <>
       <BrowserRouter>
         <Navigation />
-        {/* {isLoading && <div>Loading...</div>}
-        {!isLoading && ( */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/user/:id" element={<Profile />} />
-          </Routes>
-        {/* )} */}
+        <Layout />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user/:id" element={<Profile />} />
+        </Routes>
       </BrowserRouter>
     </>
   )
 }
 
 export default App
-
-// user={data}
-// data={data}
