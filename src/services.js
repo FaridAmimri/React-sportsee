b4a4a091d847c3857db9dbc70263ecef3726bfdf
Farid Amimri <farid.amimri@gmail.com>
2022-03-29 11:24:30 +0000

    Ajout commentaires jsDoc

--- src/services.js
+++ src/services.js
@@ -1,33 +1,30 @@
-/**
- * Service dispatching the API Call
- * @function - Dispatching data for components
- * @param {number} userId - Id of the user
- * @param {string} url - API
- * @return {object} - Using promise returning data in JSON formated Object
- */
-
 export const Service = {
-    getUserMainData: function(userId) {
-        return fetch(`http://localhost:3000/user/${userId}`)
-        .then((res) => res.json())
-        .then((result) => result.data)
-    },
+  /**
+   * @param {Number} userId - Id of the user
+   * @param {String} url - API as parameter for fetch
+   * @return {Promise} - Using promise returning data in JSON formated Object
+   */
+  getUserMainData: function (userId) {
+    return fetch(`http://localhost:3000/user/${userId}`)
+      .then((res) => res.json())
+      .then((result) => result.data)
+  },
 
-    getUserActivity: function(userId) {
-        return fetch(`http://localhost:3000/user/${userId}/activity`)
-        .then((res) => res.json())
-        .then((result) => result.data)
-    },
+  getUserActivity: function (userId) {
+    return fetch(`http://localhost:3000/user/${userId}/activity`)
+      .then((res) => res.json())
+      .then((result) => result.data)
+  },
 
-    getUserAverage: function(userId) {
-        return fetch(`http://localhost:3000/user/${userId}/average-sessions`)
-        .then((res) => res.json())
-        .then((result) => result.data)
-    },
+  getUserAverage: function (userId) {
+    return fetch(`http://localhost:3000/user/${userId}/average-sessions`)
+      .then((res) => res.json())
+      .then((result) => result.data)
+  },
 
-    getUserPerformance: function(userId) {
-        return fetch(`http://localhost:3000/user/${userId}/performance`)
-        .then((res) => res.json())
-        .then((result) => result.data)
-    }
+  getUserPerformance: function (userId) {
+    return fetch(`http://localhost:3000/user/${userId}/performance`)
+      .then((res) => res.json())
+      .then((result) => result.data)
+  },
 }
