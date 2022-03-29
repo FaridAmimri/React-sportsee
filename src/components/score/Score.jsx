c5442f7b583c67b7b5cc74a05cabd5346ad2c694
Farid Amimri <farid.amimri@gmail.com>
2022-03-29 17:58:00 +0000

    div remplacé par svg

--- src/components/score/Score.jsx
+++ src/components/score/Score.jsx
@@ -2,57 +2,12 @@ import { PieChart, Pie, Cell } from 'recharts'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-const TitleChart = styled.span`
-  position: absolute;
-  top: 30px;
-  left: 30px;
-  width: 147px;
-  color: #20253a;
-  font-size: 15px;
-  font-weight: 500;
-  z-index: 10;
-`
-
 const ScoreContainer = styled.div`
   position: relative;
   .recharts-surface {
     background-color: #fbfbfb;
     border-radius: 5px;
   }
-`
-
-const ScoreWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  top: 63px;
-  left: 58px;
-  width: 159px;
-  height: 159px;
-  background-color: green;
-  border-radius: 100%;
-  color: #20253a;
-  z-index: 10;
-`
-
-const ScoreResult = styled.p`
-  font-size: 26px;
-  font-weight: bold;
-  color: #282d30;
-  margin: 0;
-`
-
-const ScoreContent = styled.p`
-  width: 95px;
-  height: 56px;
-  font-size: 16px;
-  font-weight: 500;
-  color: #74798c;
-  margin: 0;
-  text-align: center;
-  line-height: 25px;
 `
 
 Score.propTypes = {
@@ -73,12 +28,12 @@ function Score({ scoreData }) {
 
   return (
     <ScoreContainer className="score">
-      <TitleChart>Score</TitleChart>
-      <ScoreWrapper>
-        <ScoreResult>{scoreData * 100} %</ScoreResult>
-        <ScoreContent>de votre objectif</ScoreContent>
-      </ScoreWrapper>
       <PieChart width={258} height={263}>
+        <svg>
+          <text x="30" y="40" fontSize={15} fill="#20253a">
+            Score
+          </text>
+        </svg>
         <Pie
           data={scoreDataModified}
           dataKey="value"
@@ -96,6 +51,20 @@ function Score({ scoreData }) {
             />
           ))}
         </Pie>
+        <svg>
+          <circle cx="130" cy="133" r="70" fill="#ffffff" />
+          <text x="105" y="125" fontSize={26} fontWeight="bold" fill="#282d30">
+            {scoreData * 100}%
+          </text>
+          <text fontSize={16} width="20px" fill="#74798c">
+            <tspan x="100" y="150">
+              de votre
+            </tspan>
+            <tspan x="100" y="175">
+              objectif
+            </tspan>
+          </text>
+        </svg>
       </PieChart>
     </ScoreContainer>
   )
